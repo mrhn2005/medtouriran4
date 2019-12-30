ec0ff49953350f05f705d5fda282003ace9bc939
Masoud Reyhanian <mrhn2005@gmail.com>
2019-12-30 21:34:28 +0330

    blog category model

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -3,14 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 class BlogCategory extends Model
 {
+    use Translatable;
+    
     protected $translatable = ['title'];
 
     protected $fillable = ['slug', 'title'];
 
     public function posts(){
         return $this->belongsToMany('App\Models\Post','blog_category_post');
-  } 
+    } 
+
+    public function children()
+    {
+        return $this->hasMany('App\Models\BlogCategory', 'parent_id')
+            ->with('children');
+    }
+    
+    public function parent_items()
+    {
+        return $this->hasMany('App\Models\BlogCategory')
+            ->whereNull('parent_id');
+    }
+
+    public function parentId()
+    {
+        return $this->belongsTo(self::class);
+    }
 }
