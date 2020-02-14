29d6edaef66d96530ee7ff4c075a1b4213074787
Masoud Reyhanian <mrhn2005@gmail.com>
2020-02-14 18:51:34 +0330

    stop video when not visible in home page

--- resources/assets/js/pages/home.js
+++ resources/assets/js/pages/home.js
@@ -1,9 +1,10 @@
+var youtube_iframe='<div id="main-video-intro"><iframe class="video-sample-frame--video" src="https://www.youtube.com/embed/'+site_video+'?rel=0&amp;showinfo=0" allow="accelerometer; autoplay; encrypted-media;" allowfullscreen></iframe></div>';
 $('#video-sample-frame--player').click(function(){
-
-    $('<iframe class="video-sample-frame--video" src="https://www.youtube.com/embed/'+site_video+'?rel=0&amp;showinfo=0" allow="accelerometer; autoplay; encrypted-media;" allowfullscreen></iframe>').appendTo('#introduction-video');
+    $(youtube_iframe).appendTo('#introduction-video');
     $(".video-sample-frame-player").show(500);
 });
 $('#video-sample-frame--player-close').click(function() {
+    $('#main-video-intro').remove();
     $(".video-sample-frame-player").hide(500);
 });
 $(window).resize(function () {
@@ -14,3 +15,14 @@ $(window).resize(function () {
         $('img.video-sample-frame--img').attr("data-src","/images/video_review/medtour-xs.png");
     }
 });
+
+var owl = $('#testimonial-slider');
+owl.owlCarousel();
+// Listen to owl events:
+owl.on('changed.owl.carousel', function(event) {
+    var player =$('#testimonial-slider .owl-stage-outer .owl-stage .active .video-sample-frame iframe');
+    var videoURL = player.prop('src');
+    videoURL = videoURL.replace("&autoplay=1", "");
+    player.prop('src','');
+    player.prop('src',videoURL);
+})
