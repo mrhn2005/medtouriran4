929b676d3b414b4ee57634e9c1cb3f4867528576
Masoud Reyhanian <Masoud Reyhanian>
2020-02-15 17:23:11 +0330

    firefox problem solved

--- resources/views/front/layout/includes/css-minified.blade.php
+++ resources/views/front/layout/includes/css-minified.blade.php
@@ -4,13 +4,13 @@
 <link rel="stylesheet" href="{{mix('css/app.css')}}">
 
 @if(!$is_rtl)
-    <link rel="preload" href="{{mix('css/ltr-all.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="stylesheet" rel="preload" href="{{mix('css/ltr-all.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link rel="stylesheet" href="{{mix('css/ltr-all.css')}}"></noscript>
 
 @else
-    <link rel="preload" href="{{mix('css/rtl-all.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="stylesheet" rel="preload" href="{{mix('css/rtl-all.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link rel="stylesheet" href="{{mix('css/rtl-all.css')}}"></noscript>
 @endif
 
-<link rel="preload" href="{{mix('css/languages/'.App::getLocale().'.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="stylesheet" rel="preload" href="{{mix('css/languages/'.App::getLocale().'.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link rel="stylesheet" href="{{mix('css/languages/'.App::getLocale().'.css')}}"></noscript>
