79fec5906a3bb811d2120c0ea196a6ffb32176e3
Eli_j <ejafari27@gmail.com>
2020-02-21 01:26:16 +0330

    reply reviews fixed

--- resources/views/front/common/review-form-content.blade.php
+++ resources/views/front/common/review-form-content.blade.php
@@ -2,20 +2,18 @@
 <div class="sub-ttl layer-ttl-white">@lang('home.write_review')</div>
 <div class="row comment-form py-0">
     <div class="col-sm-12">
-        <textarea name="comment" rows="4" id="package-page--comment-message" placeholder="@lang('home.your_comment')"></textarea>
-
-
+        <textarea name="comment" rows="4" id="comment-message" placeholder="@lang('home.your_comment')"></textarea>
     </div>
     <div class="col-sm-6">
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label form-input">
-            <input class="mdl-textfield__input" type="text" name="name" id="hm-booking-form--name" required>
+            <input class="mdl-textfield__input" type="text" name="comment-name" id="name-message" required>
             <label class="mdl-textfield__label" for="comment-name">@lang('home.fullname') <em> *</em></label>
             <span class="mdl-textfield__error">@lang('home.name_field')</span>
         </div>
     </div>
     <div class="col-sm-6">
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label form-input">
-            <input class="mdl-textfield__input" type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" id="hm-booking-form--email">
+            <input class="mdl-textfield__input" type="text" name="comment-email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" id="email-message">
             <label class="mdl-textfield__label" for="comment-email">@lang('home.email')</label>
             <span class="mdl-textfield__error">@lang('home.email_field')</span>
         </div>
